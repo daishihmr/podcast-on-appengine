4f12e764fc064da8be0bef9b5fc97ed6c9db555d
daishi.hmr <daishi.hmr@12d047f8-7181-5f3a-fa07-09d8e882b087>
2010-07-27 14:38:31 +0000

    git-svn-id: https://podcast-on-appengine.googlecode.com/svn/trunk/podcast-site-gae@85 12d047f8-7181-5f3a-fa07-09d8e882b087

--- test/net/hmrradio/podcastsite/controller/blogEntry/PutControllerTest.java
+++ test/net/hmrradio/podcastsite/controller/blogEntry/PutControllerTest.java
@@ -3,23 +3,53 @@ package net.hmrradio.podcastsite.controller.blogEntry;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.hmrradio.podcastsite.model.BlogEntry;
+
 import org.junit.Test;
+import org.slim3.datastore.Datastore;
 import org.slim3.tester.ControllerTestCase;
+
+import com.google.appengine.api.datastore.KeyFactory;
 
 public class PutControllerTest extends ControllerTestCase {
 
     @Test
     public void run() throws Exception {
+        tester.param("title", "タイトルだよ");
+        tester.param("content", "本文だよ");
         tester.start("/blogEntry/put");
+
         PutController controller = tester.getController();
         assertThat(controller, is(notNullValue()));
         assertThat(tester.isRedirect(), is(false));
         assertThat(tester.getDestinationPath(), is(nullValue()));
 
-        assertThat("hello", is("world"));
-    }
+        assertThat(
+            tester.response.getOutputAsString().contains("\"success\":true"),
+            is(true));
+        assertThat(
+            tester.response
+                .getOutputAsString()
+                .contains("\"title\":\"タイトルだよ\""),
+            is(true));
 
-    @Test
-    public void go() {
+        Pattern p = Pattern.compile("(\"keyString\":\"\\w+\")");
+        Matcher m = p.matcher(tester.response.getOutputAsString());
+        assertThat(m.find(), is(true));
+        assertThat(m.groupCount(), is(1));
+        String hit = m.group(0);
+        String key =
+            hit.substring("\"keyString\":\"".length(), hit.length() - 1);
+
+        BlogEntry newEntry =
+            Datastore
+                .query(BlogEntry.class, KeyFactory.stringToKey(key))
+                .asSingle();
+        assertThat(newEntry, is(notNullValue()));
+        assertThat(newEntry.getTitle(), is("タイトルだよ"));
+        assertThat(newEntry.getContent().getValue(), is("本文だよ"));
     }
 }
